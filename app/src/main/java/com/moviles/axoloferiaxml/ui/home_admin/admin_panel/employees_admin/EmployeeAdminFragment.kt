2e802a0b8146dd38fe4e85aba6967c441130ffdf
raul8500 <lopezjo299@gmail.com>
2023-11-09 21:38:03 -0600

    cambios minimos

--- app/src/main/java/com/moviles/axoloferiaxml/ui/home_admin/admin_panel/employees_admin/EmployeeAdminFragment.kt
+++ app/src/main/java/com/moviles/axoloferiaxml/ui/home_admin/admin_panel/employees_admin/EmployeeAdminFragment.kt
@@ -13,23 +13,23 @@ import android.widget.Button
 import android.widget.EditText
 import android.widget.ImageButton
 import android.widget.Spinner
-import android.widget.SpinnerAdapter
+import android.widget.Toast
 import androidx.appcompat.app.AlertDialog
 import com.moviles.axoloferiaxml.R
+import com.moviles.axoloferiaxml.core.RetrofitHelper
+import com.moviles.axoloferiaxml.data.model.user_employee.RegisterRequest
+import com.moviles.axoloferiaxml.data.network.UserAPIClient
+import com.moviles.axoloferiaxml.data.network.user_employee.EmployeeAPIClient
 
 class EmployeeAdminFragment : Fragment() {
 
-    private lateinit var spinner: Spinner
     @SuppressLint("MissingInflatedId")
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         val root = inflater.inflate(R.layout.fragment_employee_admin, container, false)
-
         val button = root.findViewById<ImageButton>(R.id.newEmployeeButton)
         button.setOnClickListener {
             showDialog()
         }
-
-
 
         return root
     }
@@ -44,12 +44,22 @@ class EmployeeAdminFragment : Fragment() {
         val dialog = builder.create()
         dialog.window!!.setBackgroundDrawable(ColorDrawable(Color.TRANSPARENT))
 
+        val spinner = dialogView.findViewById<Spinner>(R.id.rolesSpinner)
+
+        val spinnerAdapter = ArrayAdapter.createFromResource(requireContext(), R.array.roles_array, android.R.layout.simple_spinner_item)
+        spinnerAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+        spinner.adapter = spinnerAdapter
+
         val btnRegister = dialogView.findViewById<Button>(R.id.btnRegister)
+
         btnRegister.setOnClickListener {
-            val username = dialogView.findViewById<EditText>(R.id.editTextUsername).text.toString()
-            val password = dialogView.findViewById<EditText>(R.id.editTextPassword).text.toString()
-            dialog.dismiss()
+            val username2 = dialogView.findViewById<EditText>(R.id.editTextUsername).text.toString()
+            val password2 = dialogView.findViewById<EditText>(R.id.editTextPassword).text.toString()
+            val email2 = dialogView.findViewById<EditText>(R.id.editTextEmail).text.toString()
+            val rol2 = 3
+
         }
         dialog.show()
     }
+
 }
